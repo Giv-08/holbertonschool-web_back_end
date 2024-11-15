1e1863e164799246667b1c76d1db5fb28e6f298a
chutima puthachon <chutima.p8@gmail.com>
2024-11-15 17:14:55 +1100

    added task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // scoped only to the `if` block
-    const task2 = false; // scoped only to the `if` block
+    let task = true; // scoped only to the `if` block
+    let task2 = false; // scoped only to the `if` block
   }
 
   return [task, task2];
