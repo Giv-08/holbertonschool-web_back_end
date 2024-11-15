21ac76b3f44c70888f8507df7a6f173678a68f1b
chutima puthachon <chutima.p8@gmail.com>
2024-11-15 21:26:25 +1100

    added 10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 function appendToEachArrayValue(array, appendString) {
   for (let [index, value] of array.entries()) {
-    // var value = array[idx];
     array[index] = appendString + value;
   }
   return array;
