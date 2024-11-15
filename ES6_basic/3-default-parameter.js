009ebc670d896064d53186d4c526c158e8f5be57
chutima puthachon <chutima.p8@gmail.com>
2024-11-15 18:20:08 +1100

    added files

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
-export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
   // if (expansion1989 === undefined) {
   //   expansion1989 = 89;
   // }
@@ -6,5 +6,5 @@ export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, exp
   // if (expansion2019 === undefined) {
   //   expansion2019 = 19;
   // }
-  return initialNumber + expansion1989 + expansion2019;
+  return (initialNumber || 0) + (expansion1989 || 89) + (expansion2019 || 19);
 }
