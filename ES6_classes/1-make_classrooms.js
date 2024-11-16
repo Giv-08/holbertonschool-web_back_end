be6e62c379669aa0e1ed616f3684297ac07d9993
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 10:47:55 +1100

    added 1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  let array = [];
+  const array = [];
 
   const classRoomOne = new ClassRoom(19);
   const classRoomTwo = new ClassRoom(20);
   const classRoomThree = new ClassRoom(34);
 
   array.push(classRoomOne, classRoomTwo, classRoomThree);
-  return array
-};
+  return array;
+}
