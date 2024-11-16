b85133c3973305cae4d5831dfa15c578e40f7575
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 08:39:54 +1100

    fixed 10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,12 +1,11 @@
 export default function appendToEachArrayValue(array, appendString) {
   let newArr = [];
   let index = 0;
-// for in used to enumerate
 
   for (const value of array) {
     newArr[index] = appendString + value;
     index += 1;
-    // console.log(new_arr)
   }
+
   return newArrr;
 }
