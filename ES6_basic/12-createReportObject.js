77ee3b19f0bcb4a096c220a56e546183dbb882b1
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 09:15:15 +1100

    added 12-createReportObject.js

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return { allEmployees: {...employeesList},
+    getNumberOfDepartments(employeesList) {
+      const count = Object.keys(employeesList);
+      return count.length;
+    }
+  };
+}
