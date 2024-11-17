abced220c958618a63eaab00dcb0b086ea02b55f
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 21:17:07 +1100

    added 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate the SkyHighBuilding with ${this._floor} floors immediately!`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
