a77a7af0826930f49542e5d5795e16db796a655d
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 20:53:24 +1100

    added task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft!== 'number') {
+    if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
 
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
-
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
