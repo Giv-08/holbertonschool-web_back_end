5e3ef5de69bee237558d98822799d644450ac226
chutima puthachon <chutima.p8@gmail.com>
2024-11-17 22:53:40 +1100

    added 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,47 +1,22 @@
-// export default class Car {
-//   constructor(brand, motor, color) {
-//     if (typeof brand !== 'string') {
-//       throw new TypeError('Brand must be a string');
-//     }
-
-//     if (typeof motor !== 'string') {
-//       throw new TypeError('Motor must be a string');
-//     }
-
-//     if (typeof color !== 'string') {
-//       throw new TypeError('Color must be a string');
-//     }
-
-//     this._brand = brand;
-//     this._motor = motor;
-//     this._color = color;
-//   }
-
-//   cloneCar() {
-//     return new Car(this._brand, this._motor, this._color);
-//   }
-// }
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
+    // if (typeof brand !== 'string') {
+    //   throw new TypeError('Brand must be a string');
+    // }
 
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
+    // if (typeof motor !== 'string') {
+    //   throw new TypeError('Motor must be a string');
+    // }
 
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
+    // if (typeof color !== 'string') {
+    //   throw new TypeError('Color must be a string');
+    // }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    const clone = new Car(this._brand, this._motor, this._color);
-    return clone;
+    return new this.constructor
   }
 }
