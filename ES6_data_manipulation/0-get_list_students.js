0a512adc6074ec6124fd4ae5928c9556e0c6de32
chutima puthachon <chutima.p8@gmail.com>
2024-11-24 19:49:11 +1100

    added task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,5 @@
-// export default class getLstStudents {
-//   constructor(id, first_name, location) {
-//     if (typeof id !== 'number') {
-//       throw new TypeError('Id must be a number');
-//     }
-//     if (typeof first_name !== 'string') {
-//       throw new TypeError('First name must be a string');
-//     }
-//     if (typeof location !== 'string') {
-//       throw new TypeError('Location must be a string');
-//     }
-//     this._id = id;
-//     this._first_name = first_name;
-//     this._location = location;
-//   }
-// }
-
 export default function getLstStudents() {
-  let array = [
+  const array = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
