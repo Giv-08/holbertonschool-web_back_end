80182baf6ac717abe724cdda408cb77a6eb78fdf
chutima puthachon <chutima.p8@gmail.com>
2024-11-25 10:45:08 +1100

    added task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getLstStudents(arr) {
   if (!Array.isArray(arr)) {
     return [];
   }
-  const ids = arr.map((obj) => obj.id)
+  const ids = arr.map((obj) => obj.id);
   return ids; // new array
 }
