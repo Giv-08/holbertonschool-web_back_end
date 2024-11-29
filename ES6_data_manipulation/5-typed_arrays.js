85a023e8822f715b23ba2e194d1875966f5e5274
chutima puthachon <chutima.p8@gmail.com>
2024-11-29 13:13:39 +1100

    added task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const newView = new DataView(buffer);
-  newView.setInt8(position, value);
+  if (position >= length || position > 0) {
+    newView.setInt8(position, value);
+  }
   return newView;
 }
