2abd8f217e86b5256a70b78d6b3c3648c9cb57d4
chutima puthachon <chutima.p8@gmail.com>
2024-11-29 11:28:08 +1100

    added task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,15 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades = 'N/A') {
-  const filteredStudents = studentList.filter(student => {
-    return student.location === city;
-  });
+  const filteredStudents = studentList.filter((student) => student.location === city);
 
-  const updatedStudents = filteredStudents.map(student => {
-    let updatedGrades = newGrades.find((entry) => entry.studentId === student.id)
-        if (updatedGrades) {
-          student.grade = updatedGrades.grade
-        } else {
-          student.grade = 'N/A'
-        }
-      return student; // map() expects a value to be returned for each iteration
-    });
+  const updatedStudents = filteredStudents.map((student) => {
+    const updatedGrades = newGrades.find((entry) => entry.studentId === student.id);
+    const updatedStudent = { ...student };
+    if (updatedGrades) {
+      updatedStudent.grade = updatedGrades.grade;
+    } else {
+      updatedStudent.grade = 'N/A';
+    }
+    return updatedStudent; // map() expects a value to be returned for each iteration
+  });
   return updatedStudents;
 }
