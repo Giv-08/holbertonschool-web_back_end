00900b98c014ba03d593002b90e1e150b3f2fdb0
chutima puthachon <chutima.p8@gmail.com>
2024-12-01 08:59:05 +1100

    added task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
-export default function cleanSet(set, startString="") {
-  let modifiedArray = [];
+export default function cleanSet(set, startString = "") {
+  const modifiedArray = [];
 
-  if (!startString || typeof startString !== "string") {
-    return "";
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
 
   for (const value of set) {
     if (value.startsWith(startString)) {
-      let newValue = value.slice(startString.length);
+      const newValue = value.slice(startString.length);
       modifiedArray.push(newValue);
     }
   }
