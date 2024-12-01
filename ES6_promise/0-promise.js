090618a5eda31b67064c329465566435bb491af7
chutima puthachon <chutima.p8@gmail.com>
2024-12-01 13:11:26 +1100

    added task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve("fetched data successfully")
-  });
+export default async function getResponseFromAPI() {
+  return "API response";
 }
