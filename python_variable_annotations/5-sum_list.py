0fd8d60a0e20368ebfcd5c6b2307b8d281fc5c16
chutima puthachon <chutima.p8@gmail.com>
2024-12-03 11:46:15 +1100

    added task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' this module takes a list input_list of
+floats as argument and returns their sum as a float.'''
+
+
+def sum_list(input_list: list) -> float:
+    '''function that returns sum as a float.
+
+    Args:
+        input_list (list): A list of numbers.
+
+    Returns:
+        float: convert sum to float.
+    '''
+    return sum(input_list)
