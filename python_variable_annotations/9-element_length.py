cd71cf6a079e006a23c278639373000f997586af
chutima puthachon <chutima.p8@gmail.com>
2024-12-03 15:17:03 +1100

    added task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 ''' this module takes a list as
 argument and returns a list of tuple that contains int type'''
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[Iterable]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''function that returns a list of tuples containing
     the element and its length.
 
