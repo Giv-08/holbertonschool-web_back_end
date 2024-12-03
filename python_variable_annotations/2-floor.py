1c9963e09685cc3021a4df65d02a64531cc9b650
chutima puthachon <chutima.p8@gmail.com>
2024-12-03 11:01:52 +1100

    added task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+''' this module takes a float n as argument and
+returns the floor of the float'''
+
+
+def floor(floor: float) -> int:
+    '''function that returns floor of the float'''
+    return math.floor(floor)
