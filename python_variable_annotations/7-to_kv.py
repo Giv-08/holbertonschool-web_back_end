8499f7e319e498f2f2879c184b736a9e1a723256
chutima puthachon <chutima.p8@gmail.com>
2024-12-03 13:13:18 +1100

    added task7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 ''' this module takes a string k and an int
 OR float v as arguments and returns a tuple.'''
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''function that returns sum as a float.
 
     Args:
