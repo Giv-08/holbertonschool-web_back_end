404869869aea5a892001a891673e3588c69729b8
chutima puthachon <chutima.p8@gmail.com>
2024-12-03 11:08:17 +1100

    added task2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -5,5 +5,12 @@ returns the floor of the float'''
 
 
 def floor(floor: float) -> int:
-    '''function that returns floor of the float'''
+    '''function that returns floor of the float.
+
+    Args:
+        n (float): A floating-point number.
+
+    Returns:
+        int: The largest integer less then or equal to the float.
+    '''
     return math.floor(floor)
