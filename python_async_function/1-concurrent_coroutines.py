d8870e2ccfad49230b3e11890bfdb1c6142ed34f
chutima puthachon <chutima.p8@gmail.com>
2024-12-05 14:28:47 +1100

    added task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,7 @@
 ''' This module takes in an integer argument (max_delay)
 waits for a random delay seconds and eventually
 returns return the list of all the delays (float values).'''
-from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
 import asyncio
 
