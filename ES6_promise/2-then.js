23b58e9d0954f1bb1b86bb3b8af404283aaba2a8
chutima puthachon <chutima.p8@gmail.com>
2024-12-06 16:30:26 +1100

    added task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   return promise
       .then(() => {
-        console.log('Got a response from the API');
         return({
           status: 200,
           body: 'Success',
         });
       })
-      .catch(() => {
+      .catch(() => Error())
+      .finally(() => {
         console.log('Got a response from the API');
-        return Promise.reject(Error());
       });
 }
