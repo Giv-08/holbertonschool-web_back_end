67369ace7c1a6505328a5260a8e6e7237f6db027
chutima puthachon <chutima.p8@gmail.com>
2024-12-07 10:55:21 +1100

    added task 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathFunction) {
-  const queue = []
+  const queue = [];
   try {
-    const result = mathFunction()
+    const result = mathFunction();
     queue.push(result, 'Guardrail was processed');
   } catch (error) {
     queue.push(error.toString(), 'Guardrail was processed');
