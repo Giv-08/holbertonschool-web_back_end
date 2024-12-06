8eb1f7f5881e969a0a4bd98448de66d0b42c4226
chutima puthachon <chutima.p8@gmail.com>
2024-12-06 21:37:58 +1100

    added task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result_array = await Promise.allSettled([
+  const resultArray = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]);
 
-  return result_array.map((result) => {
-    return {
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
-    };
-  });
+  return resultArray.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
