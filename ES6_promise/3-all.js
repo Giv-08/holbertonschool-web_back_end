4fc33552a7f761d817af49a4e9462e5baadd8f27
chutima puthachon <chutima.p8@gmail.com>
2024-12-06 16:09:54 +1100

    added task 2

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,9 +5,8 @@ export default function getFullResponseFromAPI(success) {
         status: 200,
         body: 'Success',
       });
-      console.log('Got a response from the API')
     } else {
-      reject(Error());
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
