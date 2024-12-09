0e4930fbfb2ceb0f1d1cdb977ba9477630e47d01
chutima puthachon <chutima.p8@gmail.com>
2024-12-09 21:53:04 +1100

    added task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+""" Module for async generator task 0"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronous generator that yields 10 random float numbers
     between 0 and 10. The function waits for 1 second between each
