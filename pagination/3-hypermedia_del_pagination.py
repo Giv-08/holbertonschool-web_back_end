977bbed49cf22a494ac15916e2c67ba0f81587db
chutima puthachon <chutima.p8@gmail.com>
2024-12-10 11:25:32 +1100

    added 3-hypermedia_del_pagination.py

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ This function returns a dict showing page info"""
         if index is None:
             index = 0
 
