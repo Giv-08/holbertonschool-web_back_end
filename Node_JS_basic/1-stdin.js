4a93c9377b8ec9a3a74ba72cc90d17b3fc15aea9
chutima puthachon <chutima.p8@gmail.com>
2025-01-20 16:19:23 +1100

    added 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
-const readline = require('node:readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write(`Welcome to Holberton School, what is your name?\n`);
+
+process.stdin.on('readable', () => {
+  let input;
+  if ((input  = process.stdin.read()) !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
-rl.question(`Welcome to Holberton School, what is your name?\n`, name => {
-  console.log(`Your name is: ${name}`);
-  rl.on('close', () => {
-    console.log('This important software is now closing');
-  });
-  rl.close();
-});
+process.stdin.on('end', () => {
+  process.stdout.write(`This important software is now closing`);
+})
