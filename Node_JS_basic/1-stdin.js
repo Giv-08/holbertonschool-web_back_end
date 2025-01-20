a60b5eecfb1f222602bfa5f00a8e0eda31e6612b
chutima puthachon <chutima.p8@gmail.com>
2025-01-20 15:38:01 +1100

    added 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,11 +4,10 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-
 rl.question(`Welcome to Holberton School, what is your name?\n`, name => {
   console.log(`Your name is: ${name}`);
   rl.on('close', () => {
     console.log('This important software is now closing');
-  })
-  rl.close()
+  });
+  rl.close();
 });
