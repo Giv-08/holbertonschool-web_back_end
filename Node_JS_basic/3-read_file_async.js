3b9eca6c0a6816eaf7b7f81cfb0680a83abb6183
chutima puthachon <chutima.p8@gmail.com>
2025-01-22 15:23:11 +1100

    added task 5

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,28 @@
 const fs = require('fs').promises;
 
 async function countStudents(filePath) {
-    try {
-        const data = await fs.readFile(filePath, 'utf8');
-        const lines = data.trim().split('\n');
-        const studentsCS = [];
-        const studentsSWE = [];
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.trim().split('\n');
+    const studentsCS = [];
+    const studentsSWE = [];
 
-        lines.forEach((line) => {
-            const [firstname, lastname, age, field] = line.split(',');
+    // Iterate through the lines and process student data
+    lines.forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
 
-            if (field === 'CS') {
-                studentsCS.push(firstname);
-            } else if (field === 'SWE') {
-                studentsSWE.push(firstname);
-            }
-        });
+      if (field === 'CS') {
+        studentsCS.push(firstname);
+      } else if (field === 'SWE') {
+        studentsSWE.push(firstname);
+      }
+    });
 
-        console.log(`Number of students: ${studentsCS.length + studentsSWE.length}`);
-        console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`);
-        console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+    // Return the final list of students
+    return [...studentsCS, ...studentsSWE];
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
